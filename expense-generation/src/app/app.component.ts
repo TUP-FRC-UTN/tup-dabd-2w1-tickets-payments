382eaaa3772f79df090f35c69889c3298b489c47
406049 - Gómez Eugenio <406049@tecnicatura.frc.utn.edu.ar>
2024-10-21 01:09:31 -0300

    imports-added

--- expense-generation/src/app/app.component.ts
+++ expense-generation/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ExpenseGenerationViewComponent } from './expense-generation-view/expense-generation-view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ExpenseGenerationViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
