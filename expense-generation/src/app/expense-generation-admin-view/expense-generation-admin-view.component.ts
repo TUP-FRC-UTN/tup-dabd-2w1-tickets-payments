d568a121eb1823c36e42bcb8ae04ea9b54f71359
Agustín Ivan, Olivera <114453@tecnicatura.frc.utn.edu.ar>
2024-10-21 00:26:20 -0300

    admin view added / TO-DO

---
+++ expense-generation/src/app/expense-generation-admin-view/expense-generation-admin-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-generation-admin-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './expense-generation-admin-view.component.html',
+  styleUrl: './expense-generation-admin-view.component.css'
+})
+export class ExpenseGenerationAdminViewComponent {
+
+}
