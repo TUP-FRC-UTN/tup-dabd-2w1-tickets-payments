46c461af0468540a4efcf9c182e0ce835057c4b1
Tomas Colazo <405477@tecnicatura.frc.edu.ar>
2024-10-23 19:34:39 -0300

    app.component.ts imports

--- expense-generation/src/app/app.component.ts
+++ expense-generation/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { ExpenseGenerationViewComponent } from './expense-generation-view/expense-generation-view.component';
-
+import { routes } from './app.routes';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ExpenseGenerationViewComponent],
+  imports: [RouterOutlet, ExpenseGenerationViewComponent, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'expense-generation';
