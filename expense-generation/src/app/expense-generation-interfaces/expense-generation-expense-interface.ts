baefa82a432f0893b7f826c2d3295030540a2167
Tomas Colazo <405477@tecnicatura.frc.edu.ar>
2024-10-26 13:42:11 -0300

    update expense interface

--- expense-generation/src/app/expense-generation-interfaces/expense-generation-expense-interface.ts
+++ expense-generation/src/app/expense-generation-interfaces/expense-generation-expense-interface.ts
@@ -1,19 +1,19 @@
 export interface ExpenseGenerationExpenseInterface {
-
-    id: number;
-    ownerId : number;
-    period: string;
-    issueDate: Date;
-    first_expiration_date: string;
-    first_expiration_amount: number;
-    second_expiration_date: string;
-    second_expiration_amount: string
-    payment_method: string;
-    payment_id: string;
-    status: string;
-    selected?: boolean;
-    paymentDate?: Date; 
-    //status tipe : Pago, Pendiente, Exceptuado pero el exceptuado no lo uso
-
-
+  id: number;
+  ownerId: number;
+  period: string;
+  issueDate: Date;
+  status: string;
+  first_expiration_date: string;
+  first_expiration_amount: number;
+  second_expiration_date: string;
+  second_expiration_amount: string;
+  actual_amount: string;
+  amount_payed: string;
+  payment_method: string;
+  payment_platform: string;
+  payment_id: string;
+  selected?: boolean;
+  paymentDate?: Date;
+  //status tipe : Pago, Pendiente, Exceptuado pero el exceptuado no lo uso
 }
